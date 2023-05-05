4c55659cc883106cd2e88ca59ac0ac5ea57d3714
manojpudasaini <mpudasaini17@gmail.com>
2023-05-05 21:49:15 +0545

    feat(gallery): gallery image displayed with state handling

--- public/script.js
+++
@@ -1,6 +0,0 @@
-const carousel = document.querySelector(".carousel");
-
-const dragging = (e)=> {
-    carousel.scrollLeft = e.pageX;
-}
-carousel.addEventListener("mousemove", dragging);
--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import BookImage from "../src/assets/books.jpg";
+import SeaScapeImage from "../src/assets/seascape.jpg";
+import WindowImage from "../src/assets/window.jpg";
+import styles from "../src/styles/home.module.css";
 
-function App() {
+const App = () => {
+  const [currentImage, setCurrentImage] = useState(1);
+  const handleNextImage = () => {
+    setCurrentImage((prevState) => (prevState < 3 ? prevState + 1 : prevState));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.wrapper_main}>
+      <div className={styles.image_wrapper}>
+        {currentImage === 1 && <img src={BookImage} />}
+        {currentImage === 2 && <img src={SeaScapeImage} />}
+        {currentImage === 3 && <img src={WindowImage} />}
+      </div>
+      <div>
+        <button onClick={handleNextImage}>Next image</button>
+      </div>
+      <p>I am image no. {currentImage}</p>
     </div>
   );
-}
+};
 
 export default App;
